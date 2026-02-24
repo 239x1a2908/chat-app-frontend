2250262b7af25a16065414a40deb879ae140045f
239x1a2908 <239x1a2908@gprec.ac.in>
2026-02-24 15:19:30 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,20 @@
+import { startTransition, StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Register from './pages/Register.jsx'
+import Login from './pages/Login.jsx'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
